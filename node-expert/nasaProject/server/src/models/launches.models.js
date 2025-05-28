4cfbee27fd1e9cf76f03c1c1978b84e589f36b99
alessandro.achtenberg <alessandro.achtenberg@govoll.com>
2025-05-28 14:19:36 -0300

    create route

--- node-expert/nasaProject/server/src/models/launches.models.js
+++ node-expert/nasaProject/server/src/models/launches.models.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: "Kepler Exploraation X",
@@ -15,7 +17,13 @@ launches.set(launch.flightNumber, launch);
 
 
 function getAllLaunches () {
-    return Array.from(launches.values());   
+    return Array.from(launches.values());
+}
+
+function addNewLaunch (launch) {
+    latestFlightNumber += 1;
+    launch = Object.assign(launch, { flightNumber: latestFlightNumber });
+    launches.set(launch.flightNumber, launch);
 }
 
 module.exports = {
